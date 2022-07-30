3a24dea5e152d1accff2a9fa9b121d6d0fcd22e4
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-07-30 20:29:05 +0200

    separate components for icons

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import { HiLightningBolt } from 'react-icons/hi';
-import { BsFillCartFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import CartIcon from '../svg/CartIcon';
+import LogoIcon from '../svg/LogoIcon';
 
 const Navbar = () => {
   return (
@@ -8,26 +8,14 @@ const Navbar = () => {
       <ul className="text-black grid grid-cols-2 h-full items-center justify-center ">
         <div className="flex  items-center">
           <li className="ml-12">
-            <HiLightningBolt color="#f97316" size={48} />
+           <LogoIcon />
           </li>
           <li className=" font-bold">ShoeFury</li>
         </div>
 
         <div className="place-self-end flex gap-4 h-full items-center justify-center">
-          <svg width="0" height="0">
-            <linearGradient
-              id="blue-gradient"
-              x1="100%"
-              y1="100%"
-              x2="0%"
-              y2="0%"
-            >
-              <stop stopColor="#6dd5ed" offset="0%" />
-              <stop stopColor="#2193b0" offset="100%" />
-            </linearGradient>
-          </svg>
           <li>
-            <BsFillCartFill style={{fill: "url(#blue-gradient)"}} size={32} />
+            <CartIcon />
           </li>
           <li>
             <Link to="/add-product">Add product</Link>
