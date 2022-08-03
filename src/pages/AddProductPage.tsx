17d3e12fbac5e84a2591551c7246780cf1bd103b
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-08-03 19:41:26 +0200

    added private route wrapper

--- src/pages/AddProductPage.tsx
+++ src/pages/AddProductPage.tsx
@@ -1,25 +1,11 @@
 import AddProduct from '../components/addProduct/AddProduct';
-
-import config from '../config.json';
-import { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import IsAuth from '../components/wrappers/IsAuth';
 const AddProductPage = () => {
-  const [isAuth, setIsAuth] = useState(false);
-  useEffect(() => {
-    const isAuth = async () => {
-      const response = await fetch(`${config.backendDomain}/auth/is-auth`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + localStorage.getItem('token'),
-        },
-      });
-      const data = await response.json();
-      setIsAuth(data.isAuth);
-    };
-    isAuth();
-  }, []);
-  return isAuth ? <AddProduct /> : <Navigate to="/" />;
+  return (
+    <IsAuth>
+      <AddProduct />
+    </IsAuth>
+  );
 };
 
 export default AddProductPage;
