803e232677ef6fd8260b7b77b93d6663b9bb002a
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-09-16 13:49:43 +0200

    add sticky navbar

--- src/components/detail/productDetail/ProductPriceBox.tsx
+++ src/components/detail/productDetail/ProductPriceBox.tsx
@@ -12,10 +12,14 @@ const ProductPriceBox = ({ product }: ProductPriceProps): JSX.Element => {
     console.log("hej");
   };
   return (
-    <div className="ml-16 w-full">
-      <h1 className="text-6xl font-bold">{product.title}</h1>
-      <h3>{product.price}</h3>
-      <FormButton buttonText="Add to cart" onClickHandler={onClickHandler} />
+    <div className="ml-16 w-full grid  auto-rows-min grid-flow-row">
+      <div className="flex">
+        <h1 className="text-6xl font-bold">{product.title}</h1>
+        <h3 className="self-end ml-2 font-bold">{product.price}$</h3>
+      </div>
+      <div className="h-full">
+        <FormButton buttonText="Add to cart" onClickHandler={onClickHandler} />
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import RightSideNavbar from "components/navbar/RightsideNavbar";
 
 const Navbar = (): JSX.Element => {
   return (
-    <div className="bg-white border-b-2 drop-shadow w-full h-16 ">
+    <div className="bg-white border-b-2 drop-shadow w-full h-16 sticky top-0 z-10 ">
       <ul className="text-black grid grid-cols-2 h-full items-center justify-center ">
         <Logo />
         <RightSideNavbar />
