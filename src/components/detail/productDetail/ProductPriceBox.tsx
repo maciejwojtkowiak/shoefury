8928320dc8169d40fd5d9a5d516b1a28cc7ac8b1
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-09-16 12:49:48 +0200

    add to cart btn in detal

--- src/components/detail/productDetail/ProductPriceBox.tsx
+++ src/components/detail/productDetail/ProductPriceBox.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Product } from "types/product";
 
+import FormButton from "components/ui/buttons/FormButton";
+
 interface ProductPriceProps {
   product: Product;
 }
 
 const ProductPriceBox = ({ product }: ProductPriceProps): JSX.Element => {
+  const onClickHandler = (): void => {
+    console.log("hej");
+  };
   return (
-    <div className="ml-16">
+    <div className="ml-16 w-full">
       <h1 className="text-6xl font-bold">{product.title}</h1>
       <h3>{product.price}</h3>
+      <FormButton buttonText="Add to cart" onClickHandler={onClickHandler} />
     </div>
   );
 };
