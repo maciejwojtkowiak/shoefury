70f4f12739ffea72e51160826946f0c3e41bd44d
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-09-23 22:26:46 +0200

    fix product fetching

--- src/components/products/navigation/ProductsNavigation.tsx
+++ src/components/products/navigation/ProductsNavigation.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 
-const ProductsNavigation = (): JSX.Element => {
-  return <div></div>;
+interface ProductNavigationProps {
+  actualPage: number;
+  isAtMaxPage: boolean;
+  isAtMinPage: boolean;
+  moveBack: () => void;
+  moveForward: () => void;
+}
+
+const ProductsNavigation = ({
+  actualPage,
+  isAtMaxPage,
+  isAtMinPage,
+  moveBack,
+  moveForward,
+}: ProductNavigationProps): JSX.Element => {
+  const isAtLimitPageAttr = {
+    disabled: true,
+  };
+  return (
+    <div className="w-full flex justify-center items-center pt-24 pb-12 ">
+      <button onClick={moveBack} {...(isAtMinPage && isAtLimitPageAttr)}>
+        <MdArrowBackIos size={48} color={isAtMinPage ? "gray" : "black"} />
+      </button>
+      {actualPage}
+      <button onClick={moveForward} {...(isAtMaxPage && isAtLimitPageAttr)}>
+        <MdArrowForwardIos size={48} color={isAtMaxPage ? "gray" : "black "} />
+      </button>
+    </div>
+  );
 };
 
 export default ProductsNavigation;
