5ad32351eb0caaaf57f9ae3defa45e5ac436f63b
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-09-27 21:17:22 +0200

    axios client mocked

--- src/components/products/Products.test.tsx
+++ src/components/products/Products.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
-import axios from "axios";
+import mockAxios from "axios";
+import { IProduct } from "types/product";
 
 // import { IProduct } from "types/product";
 import ReduxProvider from "components/wrappers/ReduxProvider";
@@ -9,19 +10,23 @@ import ReduxProvider from "components/wrappers/ReduxProvider";
 import Products from "./Products";
 
 jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Test of products fetch", () => {
   test("render products", async () => {
-    mockedAxios.get.mockResolvedValueOnce({
-      data: {
-        title: "product",
-        description: "description",
-        _id: "123",
-        imageData: "",
-        price: "15",
-      },
-    });
+    (mockAxios.get as jest.Mock).mockImplementationOnce(
+      async () =>
+        await Promise.resolve({
+          data: [
+            {
+              title: "foo",
+              price: "12",
+              description: "desc",
+              _id: "123",
+              imageData: "das82e2ej",
+            },
+          ] as IProduct[],
+        }),
+    );
 
     render(
       <ReduxProvider>
