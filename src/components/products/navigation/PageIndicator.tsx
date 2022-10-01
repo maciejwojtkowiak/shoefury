8f14318c011f395ae856354c85c90c43879145f6
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-02 00:11:15 +0200

    change indicator text color

--- src/components/products/navigation/PageIndicator.tsx
+++ src/components/products/navigation/PageIndicator.tsx
@@ -13,13 +13,18 @@ const PageIndicator = ({
 }: PageIndicatorProps): JSX.Element => {
   const actualStyle = {
     style: {
-      color: "white",
       fontWeight: "900",
     },
   };
 
   return (
-    <div className="w-8 h-12 bg-gradient-to-r from-orange-300 to-orange-400 grid place-items-center rounded full mr-2 text-lg">
+    <div
+      className={`w-8 h-12 bg-gradient-to-r ${
+        actual
+          ? "from-orange-300 to-orange-400"
+          : "from-orange-200 to-orange-300"
+      } text-white grid place-items-center rounded full mr-2 text-lg`}
+    >
       <p
         {...(actual && actualStyle)}
         onClick={() => moveToPage(page)}
