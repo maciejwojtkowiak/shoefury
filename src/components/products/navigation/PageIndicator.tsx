01560f453ba259a1b43788e5c4d2908f79f73b5c
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-01 21:48:54 +0200

    styled indicators

--- src/components/products/navigation/PageIndicator.tsx
+++ src/components/products/navigation/PageIndicator.tsx
@@ -13,19 +13,21 @@ const PageIndicator = ({
 }: PageIndicatorProps): JSX.Element => {
   const actualStyle = {
     style: {
-      color: "#fb923c",
+      color: "white",
       fontWeight: "900",
     },
   };
 
   return (
-    <p
-      {...(actual && actualStyle)}
-      onClick={() => moveToPage(page)}
-      className="cursor-pointer"
-    >
-      {page}
-    </p>
+    <div className="w-8 h-12 bg-gradient-to-r from-orange-300 to-orange-400 grid place-items-center rounded full mr-2 text-lg">
+      <p
+        {...(actual && actualStyle)}
+        onClick={() => moveToPage(page)}
+        className="cursor-pointer"
+      >
+        {page}
+      </p>
+    </div>
   );
 };
 
