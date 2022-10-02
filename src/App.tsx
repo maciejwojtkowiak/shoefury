440ae3776b9681e3bb49764bda7ad45ea22d0abe
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-02 22:58:50 +0200

    private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-// import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Paths } from "config/Paths";
 import DetailPage from "pages/DetailPage";
+import { PrivateRoutes } from "utils/PrivateRoutes";
 
 import Navbar from "components/navbar/Navbar";
 
@@ -11,38 +12,41 @@ import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import Register from "./pages/Register";
+import { checkAuthentication } from "./services/authApi/checkIsAuth";
+import { userAction } from "./store/user-slice";
+import { ICheckAuthResponse } from "./types/ApiResponse";
 
-// import { checkAuthentication } from "./services/authApi/checkIsAuth";
-// import { userAction } from "./store/user-slice";
-// import { CheckAuthResponse } from "./types/ApiResponse";
-// import { IsAuthRoutes, IsUnAuthRoutes } from "./utils/PrivateRoutes";
 import "./App.css";
 
 function App(): JSX.Element {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const isAuth = async (): Promise<void> => {
-  //     const data = (await checkAuthentication(
-  //       localStorage.getItem("token"),
-  //     )) as CheckAuthResponse;
-  //     dispatch(userAction.setIsAuth(data.isAuth));
-  //   };
-  //   try {
-  //     void isAuth();
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  console.log("AUTH CHECK");
+  // TO DO: Move to axios
+  useEffect(() => {
+    const isAuth = async (): Promise<void> => {
+      const data = (await checkAuthentication(
+        localStorage.getItem("token"),
+      )) as ICheckAuthResponse;
+      console.log("DATA", data);
+      dispatch(userAction.setIsAuth(data.isAuth));
+    };
+    try {
+      void isAuth();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path={Paths.ADDPRODUCT} element={<AddProductPage />} />
+          <Route path={Paths.CART} element={<CartPage />} />
+        </Route>
         <Route path={Paths.HOME} element={<MainPage />} />
         <Route path={Paths.REGISTER} element={<Register />} />
-        <Route path={Paths.ADDPRODUCT} element={<AddProductPage />} />
-        <Route path={Paths.CART} element={<CartPage />} />
         <Route path={`${Paths.PRODUCT}/:id`} element={<DetailPage />} />
         <Route path={Paths.LOGIN} element={<LoginPage />} />
       </Routes>
