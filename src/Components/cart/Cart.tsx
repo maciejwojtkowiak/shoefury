762bf95ced2384433b14ddfd0342264af00b4a05
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-02 11:40:46 +0200

    styled cart table

--- src/Components/cart/Cart.tsx
+++ src/Components/cart/Cart.tsx
@@ -18,7 +18,7 @@ const Cart = (): JSX.Element => {
         });
         const data = await response.json();
         console.log("DATA", data);
-        setProducts(data.products.cart.items);
+        setProducts(data.products.cart);
       } catch (e) {
         console.log(e);
       }
@@ -46,9 +46,9 @@ const Cart = (): JSX.Element => {
 
   return (
     <Fragment>
-      <div className="h-screen w-full grid grid-rows-[200px_minmax(900px,_1fr)_100px]  ">
-        <div className="w-[50%] border-2 justify-self-center grid grid-rows-2 text-right ">
-          <div className="w-full">
+      <div className="w-full grid justify-content-center ">
+        <div className="w-[50%] min-h-min border-2 justify-self-center text-right mt-16 ">
+          <div className="grid auto-rows-min">
             <div className="w-full grid grid-cols-3 place-items-center mt-8 ">
               <ColumnTitle title="Product name" />
               <ColumnTitle title="Quantity" />
@@ -66,7 +66,7 @@ const Cart = (): JSX.Element => {
             })}
           </div>
           <button
-            className="border-2 justify-self-end px-16 py-2 bg-orange-300 mb-8 mr-8 text-4xl font-bold text-white "
+            className="border-2 justify-self-end px-16 py-2 bg-orange-300 mb-8 mr-8 mt-8 text-4xl font-bold text-white "
             onClick={goToCheckout}
           >
             Buy
