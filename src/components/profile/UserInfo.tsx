b1e2b9f23ba087cf28f9fd12be89007b4e9f33de
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-06 23:02:29 +0200

    add order heading

--- src/components/profile/UserInfo.tsx
+++ src/components/profile/UserInfo.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { ImPencil } from "react-icons/im";
 import shoeImg from "images/shoe.png";
 
 const UserInfo = (): JSX.Element => {
   return (
-    <div className="px-12 pt-32 bg-red-400 flex">
+    <div className="px-12 pt-32 bg-gray-100 flex items-start">
       <img src={shoeImg} className="w-[400px] h-[400px] rounded-full " />
-      <h3 className="text-4xl font-bold">Maciej Wojtkowiak</h3>
+      <div className="flex">
+        <h3 className="text-4xl font-bold">Maciej Wojtkowiak</h3>
+        <div className="self-center ml-4">
+          <ImPencil size={24} />
+        </div>
+      </div>
     </div>
   );
 };
