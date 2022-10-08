423b76aea8fab6c7577632753d979821f1c59726
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-08 20:41:25 +0200

    data is retrieved from stripe session

--- src/components/order/OrderSuccess.tsx
+++ src/components/order/OrderSuccess.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getCheckoutData } from "services/checkoutApi/checkoutApi";
 
 const OrderSuccess = (): JSX.Element => {
+  useEffect(() => {
+    const getCheckout = async (): Promise<void> => {
+      await getCheckoutData();
+    };
+    void getCheckout();
+  }, []);
   return <h1>SUCCESS</h1>;
 };
 
