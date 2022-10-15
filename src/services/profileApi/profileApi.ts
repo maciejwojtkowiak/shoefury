4ed42325f5df0c156fc55331b97a73bf5156df78
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-15 20:33:45 +0200

    working pdf

--- src/services/profileApi/profileApi.ts
+++ src/services/profileApi/profileApi.ts
@@ -13,15 +13,37 @@ export const getProfile = async (): Promise<IProfile> => {
 };
 
 export const getOrderRaport = async (orderId: string): Promise<any> => {
-  const response = await profileClient.get<any>(
+  const response = await profileClient.get<string>(
     `/get-order-raport/${orderId}`,
     {
       headers: {
-        responseType: "blob",
+        responseType: "stream",
         ...authorizationHeader(),
       },
     },
   );
-  console.log(response);
+  console.log("RESP", response.data);
+  // const file = new Blob([response.data], { type: "application/pdf" });
+  // const fileURL = URL.createObjectURL(file);
+  // window.open(fileURL);
+  // console.log("REEESPONSE", response.data);
+  // const binaryData = [];
+  // binaryData.push(response.data);
+  // const href = URL.createObjectURL(
+  //   new Blob(binaryData, { type: "application/txt" }),
+  // );
+  // const link = document.createElement("a");
+  // link.href = href;
+  // link.setAttribute("download", "file.pdf"); // or any other extension
+  // document.body.appendChild(link);
+  // link.click();
+  // document.body.removeChild(link);
+  // URL.revokeObjectURL(href);
+  const linkSource = `data:application/pdf;base64,${response.data}`;
+  const downloadLink = document.createElement("a");
+  const fileName = "abc.pdf";
+  downloadLink.href = linkSource;
+  downloadLink.download = fileName;
+  downloadLink.click();
   return response.data;
 };
