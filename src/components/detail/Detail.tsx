ea4408593272341e525531b0a2f0df747d1181e2
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-22 19:30:00 +0200

    product data is fetched using separate api endpoint

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,24 +1,23 @@
-import React, { useMemo } from "react";
-import { Navigate, useParams } from "react-router-dom";
-import { Paths } from "config/paths";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "store/hooks/reduxHooks";
-import { productsAction } from "store/products/products-slice";
-import { RootState } from "store/store";
+import { fetchProduct } from "store/products/thunks";
 
 import ProductDetail from "./productDetail/ProductDetail";
 
 const Detail = (): JSX.Element => {
-  const dispatch = useAppDispatch();
-  const products = useAppSelector(
-    (state: RootState) => state.productsReducer.products,
-  );
   const { id } = useParams();
-  const detailedProduct = useMemo(() => {
-    return products.find((product) => product._id === id);
-  }, []);
-  if (!detailedProduct) return <Navigate to={Paths.HOME} />;
+  const dispatch = useAppDispatch();
+  const detailedProduct = useAppSelector(
+    (state) => state.productsReducer.chosenProduct,
+  );
 
-  dispatch(productsAction.setChosenProduct(detailedProduct));
+  useEffect(() => {
+    const getProd = async (): Promise<void> => {
+      void dispatch(fetchProduct(id ?? ""));
+    };
+    void getProd();
+  }, []);
 
   return (
     <React.Fragment>
