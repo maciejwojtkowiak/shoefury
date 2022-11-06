a327787128aa5d17fb71a6c59bd3d290fbf1ef66
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-11-06 19:53:39 +0100

    initial docker

--- src/types/api/profile/profile.ts
+++ src/types/api/profile/profile.ts
@@ -1,7 +1,13 @@
 import { IOrder } from "types/order/Order";
 
+import { ApiResponseDefault } from "../ApiResponseDefault";
+
 export interface IProfile {
   name: string;
   profileImage: string;
   orders: Array<{ order: IOrder }>;
+}
+
+export interface IProfileEditResponse extends ApiResponseDefault {
+  name: string;
 }
